2d0b62dad4b5f3cdd2dcfc6dcffbd91f60dbb8d3
Shmulik Klein <shmulik.klein@gmail.com>
2022-07-09 21:05:28 +0200

    refactor into http.Server

--- coffee-project/main.go
+++ coffee-project/main.go
@@ -4,14 +4,26 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/shmulik-klein/spielplatz/coffee-project/handlers"
 )
 
 func main() {
 	logger := log.New(os.Stdout, "coffee-api", log.LstdFlags)
+
 	hh := handlers.NewHello(logger)
+
 	mux := http.NewServeMux()
 	mux.Handle("/hello", hh)
-	http.ListenAndServe(":9000", mux)
+
+	server := &http.Server{
+		Addr:         "9090",
+		Handler:      mux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	server.ListenAndServe()
 }
