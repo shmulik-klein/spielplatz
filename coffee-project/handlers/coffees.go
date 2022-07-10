e58c202d8cf48917e3fed93de3b9e54666a6208e
Shmulik Klein <shmulik.klein@gmail.com>
2022-07-10 21:02:23 +0200

    explict make /coffees a GET endpoint

--- coffee-project/handlers/coffees.go
+++ coffee-project/handlers/coffees.go
@@ -16,6 +16,14 @@ func NewCoffees(logger *log.Logger) *Coffees {
 }
 
 func (coffees *Coffees) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		getCoffees(rw)
+		return
+	}
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func getCoffees(rw http.ResponseWriter) {
 	coffeesList := data.GetCoffees()
 	err := coffeesList.ToJSON(rw)
 	if err != nil {
