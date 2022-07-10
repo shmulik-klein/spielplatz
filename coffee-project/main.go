7e15718caa7beb2d474693178e12a2702ca8c2ef
Shmulik Klein <shmulik.klein@gmail.com>
2022-07-10 15:28:02 +0200

    add graceful shutdown

--- coffee-project/main.go
+++ coffee-project/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/shmulik-klein/spielplatz/coffee-project/handlers"
@@ -13,17 +16,30 @@ func main() {
 	logger := log.New(os.Stdout, "coffee-api", log.LstdFlags)
 
 	hh := handlers.NewHello(logger)
-
+	coffeesHandler := handlers.NewCoffees(logger)
 	mux := http.NewServeMux()
 	mux.Handle("/hello", hh)
+	mux.Handle("/coffees", coffeesHandler)
 
 	server := &http.Server{
-		Addr:         "9090",
+		Addr:         ":9000",
 		Handler:      mux,
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
-	server.ListenAndServe()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	sig := <-sigChan
+	logger.Printf("Recieved %s signal, gracefully terminated.\n", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(tc)
 }
